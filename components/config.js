c34cc0a69f44a8f097370e8cf39e30ae970f2ba4
Simar <simranjeet09@outlook.com>
2022-11-28 22:37:26 -0500

    updated

--- components/config.js
+++ components/config.js
@@ -1,19 +1,20 @@
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXtf9QTqRX4VzgkEkgHG3xLK0G_gKxWbk",
-    authDomain: "quizapp-5b2d4.firebaseapp.com",
-    projectId: "quizapp-5b2d4",
-    storageBucket: "quizapp-5b2d4.appspot.com",
-    messagingSenderId: "1001797232174",
-    appId: "1:1001797232174:web:7096f182bb44b44ed0e1bf"
-  };
+  apiKey: 'AIzaSyBfQ6BHcrEV7Ez2CD7wdcZLOKDpV0j8CtY',
+  authDomain: 'quizapp-99bdb.firebaseapp.com',
+  databaseURL: 'https://quizapp-99bdb-default-rtdb.firebaseio.com',
+  projectId: 'quizapp-99bdb',
+  storageBucket: 'quizapp-99bdb.appspot.com',
+  messagingSenderId: '271954217712',
+  appId: '1:271954217712:web:12d49131a7d465845b1035',
+  measurementId: 'G-4NE4PV4WBM',
+};
 
-  if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-  }
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-  export { firebase };
+export {firebase};
--- components/register/Register.js
+++ components/register/Register.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,44 +8,30 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Alert,
-} from "react-native";
-import { firebase } from '../config';
+} from 'react-native';
+import {firebase} from '../config';
 
-export default function SignUpScreen({ navigation }) {
+export default function SignUpScreen({navigation}) {
+  const [name, setname] = useState('');
+  const [telephone, settelephone] = useState('');
+  const [email, setemail] = useState('');
+  const [password, setpassword] = useState('');
 
-
-  const [name, setname] = useState("");
-  const [telephone, settelephone] = useState("");
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
-
-
-  registerUser = async(name,telephone,email,password) => {
-
-    await firebase.auth().createUserWithEmailAndPassword(email,password)
-    .then(() => {
-      Alert.alert("Congratulations!","You are registered successfully");
-      navigation.navigate('Home');
-      }).catch((error) => {
-        Alert.alert(error.message);
+  registerUser = async (name, telephone, email, password) => {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Congratulations!', 'You are registered successfully');
+        navigation.navigate('Home');
       })
-
-    // await firebase.firestore().collection('/users').add({
-    //   name,
-    //   telephone,
-    //   email,
-    //   password,
-    // })
-    // .then(() => {
-    //   Alert.alert("User registered successfully!");
-    //   navigation.navigate('Home');
-    // }).catch((error) => {
-    //   Alert.alert(error.message);
-    // })
-  }
+      .catch(error => {
+        Alert.alert(error.message);
+      });
+  };
 
   return (
-    <SafeAreaView style={styles.container} >
+    <SafeAreaView style={styles.container}>
       <View style={styles.Heading}>
         <Text style={styles.titleText}>Register Now</Text>
       </View>
@@ -56,7 +42,7 @@ export default function SignUpScreen({ navigation }) {
             onChangeText={setname}
             value={name}
             placeholder="Name"
-            placeholderTextColor={"silver"}
+            placeholderTextColor={'silver'}
           />
         </View>
         <View style={styles.Row}>
@@ -65,7 +51,7 @@ export default function SignUpScreen({ navigation }) {
             onChangeText={settelephone}
             value={telephone}
             placeholder="Telephone"
-            placeholderTextColor={"silver"}
+            placeholderTextColor={'silver'}
           />
         </View>
         <View style={styles.Row}>
@@ -74,7 +60,7 @@ export default function SignUpScreen({ navigation }) {
             onChangeText={setemail}
             value={email}
             placeholder="Email"
-            placeholderTextColor={"silver"}
+            placeholderTextColor={'silver'}
           />
         </View>
         <View style={styles.Row}>
@@ -84,12 +70,16 @@ export default function SignUpScreen({ navigation }) {
             value={password}
             secureTextEntry={true}
             placeholder="Password"
-            placeholderTextColor={"silver"}
+            placeholderTextColor={'silver'}
           />
         </View>
         <View style={styles.Row}>
-          <TouchableOpacity style={styles.button} onPress={()=> {registerUser(name,telephone,email,password)}} >
-            <Text style={{ color: "white", fontSize: 16 }}>Sign Up</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              registerUser(name, telephone, email, password);
+            }}>
+            <Text style={{color: 'white', fontSize: 16}}>Sign Up</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -100,49 +90,49 @@ export default function SignUpScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   Heading: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   Rows: {
     margin: 20,
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   Row: {
     margin: 12,
   },
   titleText: {
     fontSize: 35,
-    fontWeight: "bold",
-    color: "#1B2D58",
+    fontWeight: 'bold',
+    color: '#1B2D58',
   },
   input: {
     height: 35,
     borderWidth: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 5,
-    color: "black",
-    borderColor: "silver",
+    color: 'black',
+    borderColor: 'silver',
     width: 270,
     fontSize: 14,
     padding: 5,
   },
   button: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#1B2D58",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1B2D58',
     padding: 10,
     borderRadius: 5,
     height: 40,
   },
   forgotPassword: {
-    color: "white",
+    color: 'white',
     fontSize: 17,
-    position: "absolute",
+    position: 'absolute',
     right: 5,
     top: 5,
   },
